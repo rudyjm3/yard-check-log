4c39a6278609716771009cc14684db5cd32ff6c0
Rudolph <rudyjm3@yahoo.com>
2024-10-01 15:54:19 -0400

    Display layout fixes for main-content-section, side menu li's, main-content-container, #equipment-management form, and body. Updated db-conn-info.php file with development enviroment credentials.

--- db-conn-info.php
+++ db-conn-info.php
@@ -1,10 +1,18 @@
 <?php
 // db_connection.php
+//Database connection details
 
+// Local Development database
 $servername = "localhost";
-$username = "rudyjm33"; // Replace with your actual username
-$password = "Matrix_1645"; // Replace with your actual password
+$username = "root";
+$password = "";
 $dbname = "u207292155_yard_check_db";
+
+// Live server database
+// $servername = "localhost";
+// $username = "rudyjm33"; // Replace with your actual username
+// $password = "Matrix_1645"; // Replace with your actual password
+// $dbname = "u207292155_yard_check_db";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
